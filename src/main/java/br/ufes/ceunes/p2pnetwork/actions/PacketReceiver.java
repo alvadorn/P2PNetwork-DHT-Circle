cdf125927bc0e4dc0d96cac9afe11904a8f0bc35
Igor Santana <igorba2005@gmail.com>
2015-09-29 18:50:51 -0300

    Packet receiver

---
+++ src/main/java/br/ufes/ceunes/p2pnetwork/actions/PacketReceiver.java
@@ -0,0 +1,89 @@
+package br.ufes.ceunes.p2pnetwork.actions;
+
+import java.io.ByteArrayInputStream;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Queue;
+
+import br.ufes.ceunes.p2pnetwork.model.Host;
+
+public class PacketReceiver {
+
+	private Host host;
+	private Host successor;
+	private Host antecessor;
+	private Queue<DatagramPacket> packets;
+	private final int port = 12345;
+
+	public PacketReceiver(Host host, Host successor, Host antecessor, Queue<DatagramPacket> queue) {
+		this.host = host;
+		this.successor = successor;
+		this.antecessor = antecessor;
+		packets = queue;
+	}
+
+	public void requireLookUp(ByteArrayInputStream stream) {
+		byte[] buffer = new byte[4];
+		stream.read(buffer, 0, 4);
+		long originId = Converter.bytesToUnsignedInt(buffer);
+		stream.read(buffer, 0, 4);
+		InetAddress originIp = null;
+		try {
+			originIp = InetAddress.getByAddress(buffer);
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		stream.read(buffer, 0, 4);
+		long lookingId = Converter.bytesToUnsignedInt(buffer);
+
+		boolean make = false;
+
+		if (host.getId() > successor.getId()) {
+			if (host.getId() <= lookingId || successor.getId() > lookingId) {
+				make = true;
+			}
+		} else if (host.getId() >= lookingId && successor.getId() < lookingId) {
+			make = true;
+		}
+
+		if (make) {
+			packets.add(PacketFactory.createAnswerLookUp(originIp, port, (int) lookingId, (int) successor.getId(),
+					successor.getIp()));
+		} else {
+			packets.add(PacketFactory.createRequireLookUp(successor.getIp(), port, (int) originId, originIp,
+					(int) lookingId));
+		}
+
+	}
+
+	public static void requireJoin() {
+
+	}
+
+	public static void requireLeave() {
+
+	}
+
+	public static void requireUpdate() {
+
+	}
+
+	public static void answerLookUp() {
+
+	}
+
+	public static void answerJoin() {
+
+	}
+
+	public static void answerLeave() {
+
+	}
+
+	public static void answerUpdate() {
+
+	}
+
+}
